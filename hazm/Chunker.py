9d0f1a6dfc38d06e3f5134f84f11e69ac92319d9
Alireza Nourian <alireza.nournia@gmail.com>
2014-07-17 23:05:53 +0430

    fix VP and ADJP in chunker

--- hazm/Chunker.py
+++ hazm/Chunker.py
@@ -8,26 +8,53 @@ class Chunker(RegexpParser):
 	"""
 		>>> tree2brackets(chunker.parse([('نامه', 'Ne'), ('۱۰', 'NUM'), ('فوریه', 'Ne'), ('شما', 'PRO'), ('را', 'POSTP'), ('دریافت', 'N'), ('داشتم', 'V'), ('.', 'PUNC')]))
 		'[نامه ۱۰ فوریه شما را NP] [دریافت داشتم VP] .'
+
+
+		# VP
+
+		>>> tree2brackets(chunker.parse([('تقدیم', 'Ne'), ('حضورتان', 'N'), ('نمایم', 'V')]))
+		'[تقدیم حضورتان NP] [نمایم VP]'
+
+		>>> tree2brackets(chunker.parse([('به', 'P'), ('سه', 'NUM'), ('رأی', 'N'), ('افزایش', 'N'), ('می‌یابد', 'V')]))
+		'[به PP] [سه رأی NP] [افزایش می‌یابد VP]'
+
+		>>> tree2brackets(chunker.parse([('به', 'P'), ('انجام', 'N'), ('برسانیم', 'V')]))
+		'[به انجام برسانیم VP]'
+
+
+		# ADJ
+
+		>>> tree2brackets(chunker.parse([('از', 'P'), ('خط', 'Ne'), ('جبهه', 'Ne'), ('اولیه', 'AJ'), ('ناگزیر', 'AJ'), ('به', 'P'), ('فرود', 'Ne'), ('اجباری', 'AJ'), ('شده‌اند', 'V')]))
+		'[از PP] [خط جبهه اولیه NP] [ناگزیر به ADJP] [فرود اجباری NP] [شده‌اند VP]'
+
+		>>> tree2brackets(chunker.parse([('پیروزی', 'Ne'), ('مشترکمان', 'AJ'), ('بر', 'P'), ('ستمگران', 'Ne')]))
+		'[پیروزی مشترکمان NP] [بر PP] [ستمگران NP]'
+
 	"""
 
 	def __init__(self):
 		grammar = r"""
 
 			VP:
-				{<N>?<V>}
+				{<P><N><V>}
+				<.*[^e]>{<N><V>}
+				{<V>}
 
 			ADVP:
 				{<ADV><AJ>?}
+
+			ADJP:
+				<.*[^e]>{<AJe?><Pe?>}
 
 			NP:
 				{<DETe?|Ne?|NUMe?|AJe|PRO|CL|RESe?><DETe?|Ne?|NUMe?|AJe?|PRO|POSTP|CL|RESe?>*}
 				<N>}{<Ne?>
 
-			PP:
-				{<Pe?>}
-
 			ADJP:
-				{<AJe?><Pe?>}
+				{<AJe?>}
+
+			PP:
+				{<Pe?>+}
 
 		"""
 
@@ -45,5 +72,8 @@ def tree2brackets(tree):
 			tag = item[2].split('-')[1]
 			str += '['
 		str += item[0] +' '
+
+	if tag:
+		str += tag +'] '
 
 	return str.strip()
