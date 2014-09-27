94ab4a49472ef93f9e5fd4830d61f412343749c1
Alireza Nourian <az.nourian@gmail.com>
2014-09-27 19:51:08 +0330

    fix dependency parser default tagger

--- hazm/DependencyParser.py
+++ hazm/DependencyParser.py
@@ -9,7 +9,7 @@ from nltk.parse.malt import MaltParser
 
 
 class DependencyParser(MaltParser):
-	def __init__(self, tagger, lemmatizer=Lemmatizer(), model_file='langModel.mco', working_dir='resources'):
+	def __init__(self, tagger=POSTagger(), lemmatizer=Lemmatizer(), model_file='langModel.mco', working_dir='resources'):
 		os.environ['MALT_PARSER'] = working_dir
 		super(DependencyParser, self).__init__(tagger=tagger, mco=model_file, working_dir=working_dir)
 		self.lemmatize = lemmatizer.lemmatize if lemmatizer else lambda w, t: '_'
