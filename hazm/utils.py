d8a3253d6a32e3107128b336dcef2c3711109d74
ehsan-mohammadi <ehsan.tck@gmail.com>
2015-09-25 02:51:49 +0330

    Update utils.py

--- hazm/utils.py
+++ hazm/utils.py
@@ -8,7 +8,7 @@ data_path = path.join(path.dirname(__file__), 'data')
 default_words = path.join(data_path, 'words.dat')
 default_verbs = path.join(data_path, 'verbs.dat')
 
-def FreqDict(text):
+def freq_dict(text):
     words = Counter()
     words.update(text.split())
     return words.most_common()
