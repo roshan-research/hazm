71ded9e8e48c859e79a86c4255302d28a55bc836
Alireza Nourian <az.nourian@gmail.com>
2017-07-13 19:26:29 +0430

    fix zwnj in token splitter

--- hazm/TokenSplitter.py
+++ hazm/TokenSplitter.py
@@ -19,9 +19,17 @@ class TokenSplitter():
 		[('صد', 'او'), ('صدا', 'و')]
 		>>> splitter.split_token_words('شهرموشها')
 		[('شهر', 'موشها')]
+		>>> splitter.split_token_words('داستان‌سرا')
+		[('داستان', 'سرا'), ('داستان‌سرا',)]
+		>>> splitter.split_token_words('دستان‌سرا')
+		[('دستان', 'سرا')]
 		"""
 
-		candidates = [(token[:s], token[s:]) for s in range(1, len(token))] + [(token, )]
-		candidates = list(filter(lambda tokens: set(map(self.lemmatize, tokens)).issubset(self.words), candidates))
+		candidates = []
+		if '‌' in token:
+			candidates.append(tuple(token.split('‌')))
+
+		splits = [(token[:s], token[s:]) for s in range(1, len(token)) if token[s-1] != '‌' and token[s] != '‌'] + [(token, )]
+		candidates.extend(list(filter(lambda tokens: set(map(self.lemmatize, tokens)).issubset(self.words), splits)))
 
 		return candidates
