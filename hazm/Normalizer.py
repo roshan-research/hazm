32fbf177c6af3980eeda0efbb7e35b12e9d64e49
Alireza Nourian <az.nourian@gmail.com>
2017-10-02 15:31:44 +0330

    update affix detection pattern

--- hazm/Normalizer.py
+++ hazm/Normalizer.py
@@ -66,7 +66,7 @@ class Normalizer(object):
 			self.affix_spacing_patterns = compile_patterns([
 				(r'([^ ]ه) ی ', r'\1‌ی '),  # fix ی space
 				(r'(^| )(ن?می) ', r'\1\2‌'),  # put zwnj after می, نمی
-				(r'(?<=[^\n'+ punc_after + punc_before +']) (تر(ین?)?|گری?|های?)(?=[ \n'+ punc_after + punc_before +']|$)', r'‌\1'),  # put zwnj before تر, تری, ترین, گر, گری, ها, های
+				(r'(?<=[^\n\d '+ punc_after + punc_before +']{2}) (تر(ین?)?|گری?|های?)(?=[ \n'+ punc_after + punc_before +']|$)', r'‌\1'),  # put zwnj before تر, تری, ترین, گر, گری, ها, های
 				(r'([^ ]ه) (ا(م|یم|ش|ند|ی|ید|ت))(?=[ \n'+ punc_after +']|$)', r'\1‌\2'),  # join ام, ایم, اش, اند, ای, اید, ات
 			])
 
