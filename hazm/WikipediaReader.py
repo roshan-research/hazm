2971829c80bf9f253be2b37974dd0435f06e2a24
Alireza Nourian <az.nourian@gmail.com>
2018-02-27 14:18:29 +0330

    add number of processes option to wikipedia extractor

--- hazm/WikipediaReader.py
+++ hazm/WikipediaReader.py
@@ -9,12 +9,13 @@ class WikipediaReader():
 	interfaces [Persian Wikipedia dump](http://download.wikimedia.org/fawiki/latest/fawiki-latest-pages-articles.xml.bz2)
 	"""
 
-	def __init__(self, fawiki_dump):
+	def __init__(self, fawiki_dump, n_jobs=2):
 		self.fawiki_dump = fawiki_dump
 		self.wiki_extractor = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'WikiExtractor.py')
+		self.n_jobs = n_jobs
 
 	def docs(self):
-		proc = subprocess.Popen(['python', self.wiki_extractor, '--no-templates', '--output', '-', self.fawiki_dump], stdout=subprocess.PIPE)
+		proc = subprocess.Popen(['python', self.wiki_extractor, '--no-templates', '--processes', str(self.n_jobs), '--output', '-', self.fawiki_dump], stdout=subprocess.PIPE)
 		doc_pattern = re.compile(r'<doc id="(\d+)" url="([^\"]+)" title="([^\"]+)">')
 
 		doc = []
