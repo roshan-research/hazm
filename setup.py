d1e43ae9dad7e84e9b269caea2c51be8fd98274a
Alireza Nourian <az.nourian@gmail.com>
2018-10-12 13:36:10 +0330

    remvoe readme rst

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 
+import codecs
+from os import path
 from setuptools import setup
+
 
 setup(
 	name='hazm',
-	version='0.6.0.9',
+	version='0.6.0.5',
 	description='Python library for digesting Persian text.',
 	author='Alireza Nourian',
 	author_email='az.nourian@gmail.com',
 	url='http://www.sobhe.ir/hazm/',
+	long_description=codecs.open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8').read(),
+	long_description_content_type='text/markdown',
 	packages=['hazm'],
 	package_data={'hazm': ['data/*.dat']},
 	classifiers=[
