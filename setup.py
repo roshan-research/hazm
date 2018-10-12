e3c34b95639962e1f8c09097b9c68ab1d49ea919
Alireza Nourian <az.nourian@gmail.com>
2018-10-12 14:13:32 +0330

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
 	name='hazm',
-	version='0.6.0.5',
+	version='0.6.0.0',
 	description='Python library for digesting Persian text.',
 	author='Alireza Nourian',
 	author_email='az.nourian@gmail.com',
