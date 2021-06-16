ba7bb1cb3ffd226befbca8cf291a0ae842b02d4b
Patrick Simianer <patrick@lilt.com>
2021-06-16 10:45:20 +0200

    requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
 	name='hazm',
-	version='0.7.1',
+	version='0.7.1a',
 	description='Python library for digesting Persian text.',
 	author='Alireza Nourian',
 	author_email='az.nourian@gmail.com',
@@ -18,14 +18,14 @@ setup(
 	classifiers=[
 		'Topic :: Text Processing',
 		'Natural Language :: Persian',
-		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3.4',
 		'Programming Language :: Python :: 3.5',
 		'Programming Language :: Python :: 3.6',
 		'Programming Language :: Python :: 3.7',
 		'Programming Language :: Python :: 3.8',
+		'Programming Language :: Python :: 3.9',
 		'License :: OSI Approved :: MIT License',
 	],
-	install_requires=['nltk==3.4', 'libwapiti>=0.2.1;platform_system!="Windows"'],
+	install_requires=['nltk>=3.4.5', 'libwapiti>=0.2.1;platform_system!="Windows"'],
 	extras_require={'wapiti': ['libwapiti>=0.2.1']},
 )
