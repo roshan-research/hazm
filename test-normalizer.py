803c8ae398eb7800aaf0a6db0a31d2f79280e857
Ayub <mrkou65@gmail.com>
2022-11-17 12:27:56 +0330

    Test normalizer with extended verbs list

---
+++ test-normalizer.py
@@ -0,0 +1,19 @@
+from hazm import *
+
+# Testing normalizer
+
+normalizer = Normalizer()
+
+worked_as_expected = []
+
+with open("normalizer-test-cases.txt", encoding='utf-8') as file:
+    while (line := file.readline().rstrip()):
+        splitted = line.split('#')
+        input = splitted[0]
+        expected = splitted[1]        
+        normal = normalizer.normalize(input)
+        if normal==expected:
+            worked_as_expected.append(line)            
+
+with open("worked_as_expected.txt", "w", encoding="utf-8") as outfile:
+    outfile.write("\n".join(worked_as_expected))
