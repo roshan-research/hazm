f9c1b3236b9c891c74683aa8d770de7e0799b497
Ayub <mrkou65@gmail.com>
2022-11-29 11:48:07 +0330

    Normal combination of chars and nums

--- hazm/Normalizer.py
+++ hazm/Normalizer.py
@@ -88,6 +88,8 @@ class Normalizer(object):
                  r'\1 \2'),  # put space after
                 ('([^ ' + punc_before + '])([' + punc_before + '])',
                  r'\1 \2'),  # put space before
+                ('(\d)([آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی])', r'\1 \2'),
+                ('([آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی])(\d)', r'\1 \2'),
             ])
 
         if affix_spacing:
