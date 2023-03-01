d1659888674d62c8f8e3ca8360f4ba5fe36917fa
imani <imani@users.noreply.github.com>
2023-03-01 10:48:30 +0000

    Refactor code based on pep-8

--- hazm/PersianPlainTextReader.py
+++ hazm/PersianPlainTextReader.py
@@ -10,10 +10,20 @@ class PersianPlainTextReader(PlaintextCorpusReader):
     are assumed to be split using blank lines.  Sentences and words can
     be tokenized using the default tokenizers, or by custom tokenizers
     specificed as parameters to the constructor.
-
+    
     """
+
     CorpusView = StreamBackedCorpusView
 
-    def __init__(self, root, fileids, word_tokenizer=word_tokenize, sent_tokenizer=sent_tokenize,
-                 para_block_reader=read_blankline_block, encoding='utf8'):
-        super().__init__(root, fileids, word_tokenizer, sent_tokenizer, para_block_reader, encoding)
+    def __init__(
+        self,
+        root,
+        fileids,
+        word_tokenizer=word_tokenize,
+        sent_tokenizer=sent_tokenize,
+        para_block_reader=read_blankline_block,
+        encoding="utf8",
+    ):
+        super().__init__(
+            root, fileids, word_tokenizer, sent_tokenizer, para_block_reader, encoding
+        )
