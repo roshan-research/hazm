90be5755e470617790883a0ad97a92d767c8c621
E_Ghafour <e.ghafour2000@gmail.com>
2023-04-09 16:26:41 +0330

    some of the functions are basicaly implemented

--- hazm/SequenceTagger.py
+++ hazm/SequenceTagger.py
@@ -6,8 +6,12 @@
 
 from __future__ import unicode_literals
 from pycrfsuite import Tagger, Trainer
+import numpy as np
+import time
+import multiprocessing
 import warnings
 
+punctuations_list = ['"', '#', '(', ')', '*', ',', '-', '.', '/', ':', '[', ']', '«', '»', '،',';','?','!']
 
 class SequenceTagger():
     """این کلاس شامل توابعی برای برچسب‌گذاری توکن‌ها است. این کلاس در نقش یک
@@ -19,16 +23,62 @@ class SequenceTagger():
     
     """
 
-    def __init__(self, model = None):
+    def __init__(self, model=None, universal_tag=False):
+        self.is_universal = universal_tag
         if model != None:
             self.load_model(model)
             
     
     def load_model(self, model):
         self.model = Tagger().open(model)
+
+    def __is_punc(self, word):
+        return word in punctuations_list
+    
+    def __universal_converter(self, tagged_list):
+        return [(word, tag.split(',')[0]) for word, tag in tagged_list]
+
+    def __features(self, sentence, index):
+        return {
+        'word': sentence[index],
+        'is_first': index == 0,
+        'is_last': index == len(sentence) - 1,
+        
+        'prefix-1': sentence[index][0],
+        'prefix-2': sentence[index][:2],
+        'prefix-3': sentence[index][:3],
+        'suffix-1': sentence[index][-1],
+        'suffix-2': sentence[index][-2:],
+        'suffix-3': sentence[index][-3:],
+        #word
+        'prev_word': '' if index == 0 else sentence[index - 1],
+        'two_prev_word':'' if index == 0 else sentence[index - 2],
+        'next_word': '' if index == len(sentence) - 1 else sentence[index + 1],
+        'two_next_word': '' if (index == len(sentence) - 1 or index == len(sentence) - 2) else sentence[index + 2],
+        #digit
+        'is_numeric': sentence[index].isdigit(),
+        'prev_is_numeric': '' if index == 0 else sentence[index - 1].isdigit(),
+        'next_is_numeric': '' if index == len(sentence) - 1 else sentence[index + 1].isdigit(),
+        #punc
+        'is_punc': self.__is_punc(sentence[index]),
+        'prev_is_punc':  '' if  index==0 else self.__is_punc(sentence[index-1]),
+        'next_is_punc':  '' if index== len(sentence) -1 else self.__is_punc(sentence[index+1]),
+    }
+
+
+    def make_data(self, tagged_list):
+        X, y = [], []
+        tagged_list = np.array(tagged_list)
+        words = tagged_list[:, 0]
+
+        for tagged in tagged_list:
+            X.append([self.__features(words, index) for index in range(len(tagged))])
+            y.append([tag for _, tag in tagged])
+
+        return X,y
         
 
-    def train(self, algouritm='lbfgs', c1=0.4, c2=0.04):
+    def train(self, tagged_list, algouritm='lbfgs', c1=0.4, c2=0.04, max_iteration=400, verbose=True, file_name='crf.model', data_maker=make_data, report_duration=True):
         """لیستی از جملات را می‌گیرد و بر اساس آن مدل را آموزش می‌دهد.
         
         هر جمله، لیستی از `(توکن، برچسب)`هاست.
@@ -41,10 +91,36 @@ class SequenceTagger():
             sentences (List[List[Tuple[str,str]]]): جملاتی که مدل از روی آن‌ها آموزش می‌بیند.
         
         """
-        self.model.train(
-            ["\n".join([" ".join(word) for word in sentence]) for sentence in sentences]
-        )
+        trainer = Trainer(verbose=verbose)
+        trainer.set_params({
+        'algourithm': algouritm,
+        'c1': c1,
+        'c2': c2,  
+        'max_iterations': max_iteration,
+        'feature.possible_transitions': True,
+        })
 
+        start_time = time.time()
+        X, y = data_maker(tagged_list)
+
+        for xseq, yseq in zip(X, y):
+            trainer.append(xseq, yseq)
+
+        end_time = time.time()
+        data_preprocessing_time = end_time - start_time
+
+        if(report_duration):
+            print(f'preprocessing time: {data_preprocessing_time}')
+        
+        start_time = time.time()
+        trainer.train(file_name)
+        end_time = time.time()
+        training_time = end_time - start_time
+
+        if(report_duration):
+            print(f'training time: {training_time}')
+            print(f'the total time duration: {data_preprocessing_time + training_time}')
+        
     def save_model(self, filename):
         """مدل تهیه‌شده توسط تابع [train()][hazm.SequenceTagger.SequenceTagger.train]
         را ذخیره می‌کند.
@@ -60,7 +136,7 @@ class SequenceTagger():
         """
         self.model.save(filename)
 
-    def tag(self, tokens):
+    def tag(self, tokens, data_maker = make_data):
         """یک جمله را در قالب لیستی از توکن‌ها دریافت می‌کند و در خروجی لیستی از
         `(توکن، برچسب)`ها برمی‌گرداند.
         
@@ -76,9 +152,18 @@ class SequenceTagger():
             (List[Tuple[str,str]]): ‌لیستی از `(توکن، برچسب)`ها.
         
         """
-        return self.tag_sents([tokens])[0]
+        return self.model.tag(data_maker(tokens)) if self.is_universal else self.__universal_converter(self.model.tag(data_maker(tokens)))
+        # if(self.is_universal):
+        #     return self.model.tag(data_maker(tokens))
+        # else:
+        #     return self.__universal_converter(self.model.tag(data_maker(tokens)))
 
-    def tag_sents(self, sentences):
+    def __tag_sents(self, sentences, ):
+        if(self.is_universal):
+            
+        
+
+    def tag_sents(self, sentences, workers = multiprocessing.cpu_count() - 1):
         """جملات را در قالب لیستی از توکن‌ها دریافت می‌کند
         و در خروجی، لیستی از لیستی از `(توکن، برچسب)`ها برمی‌گرداند.
         
@@ -97,13 +182,9 @@ class SequenceTagger():
                     هر لیست از `(توکن،برچسب)`ها مربوط به یک جمله است.
         
         """
-        sentences = list(sentences)
-        lines = "\n\n".join(["\n".join(sentence) for sentence in sentences]).replace(
-            " ", "_"
-        )
-        results = self.model.label_sequence(lines).decode("utf8")
-        tags = iter(results.strip().split("\n"))
-        return [[(word, next(tags)) for word in sentence] for sentence in sentences]
+        workers = 1 if workers == 0 else workers
+        
+        
 
 
 class IOBTagger(SequenceTagger):
