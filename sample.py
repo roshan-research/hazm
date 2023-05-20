bd792ebaf65b280ebc44a99059a4d0022c5f68b4
Ayub <sir.kokabi@gmail.com>
2023-05-20 14:19:16 +0330

    remove sample.py

--- sample.py
+++
@@ -1,8 +0,0 @@
-from hazm import pos_tagger
-
-
-from hazm import POSTagger
-from hazm import word_tokenize
-
-tagger = POSTagger(model="resources/pos_tagger.model")
-print(tagger.tag(word_tokenize("ما بسیار کتاب می‌خوانیم")))
