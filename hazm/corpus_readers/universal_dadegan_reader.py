223d0f714ea2358935c6aa8f4a65439de74e8906
E_Ghafour <e.ghafour2000@gmail.com>
2023-07-04 15:07:36 +0330

    add conll2conllu function to the universal dadegan reader

--- hazm/corpus_readers/universal_dadegan_reader.py
+++ hazm/corpus_readers/universal_dadegan_reader.py
@@ -1,6 +1,35 @@
-
 from typing import Iterator
 from hazm import DadeganReader
+import sys
+
+def conllu2conll(conllu_path: str) -> str :
+    reader1 = open(conllu_path, 'r')
+
+    delex = False
+    if len(sys.argv) > 3 and sys.argv[3] == "delex":
+        delex = True
+
+    line1 = reader1.readline()
+    
+    lines = []
+    while line1:
+        if len(line1.strip()) == 0:
+            lines.append(line1)
+        else:
+            spl = line1.strip().split('\t')
+            if len(spl) > 2:
+                if not '.' in spl[0] and spl[0].isdigit():
+                    if ':' in spl[7]:
+                        spl[7] = spl[7][:spl[7].rfind(':')]
+                    if spl[6] == '_' or spl[6] == '-':
+                        spl[6] = '-1'
+                    if delex:
+                        spl[1] = "_"
+                        spl[2] = "_"
+                    lines.append('\t'.join(spl) + '\n')
+
+        line1 = reader1.readline()
+    return ''.join(lines)
 
 class UniversalDadeganReader(DadeganReader):
     """این کلاس شامل توابعی برای خواندن پیکرهٔ PerDT است.
@@ -19,3 +48,5 @@ class UniversalDadeganReader(DadeganReader):
         Yields:
             (str): جملهٔ بعدی.
         """
+        pass
+
