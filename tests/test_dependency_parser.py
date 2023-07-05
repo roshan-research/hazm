3b57d23bbf0ad4984772b4acfa2d9c82c8fb9fe6
E_Ghafour <e.ghafour2000@gmail.com>
2023-07-05 15:43:41 +0330

    fix the test of the dependency parser

--- tests/test_dependency_parser.py
+++ tests/test_dependency_parser.py
@@ -1,6 +1,9 @@
+from hazm import word_tokenize
 class TestDependencyParser:
 
     def test_parse(self:"TestDependencyParser", dependency_parser):
-        actual = str(dependency_parser.parse(["من", "به", "مدرسه", "رفته بودم", "."]).tree())
-        expected = "(من (به (مدرسه (رفته_بودم .))))"
+        sent = 'آدمک آخر دنیاست بخند.'
+        parsed_tree = dependency_parser.parse(word_tokenize(sent))
+        actual = parsed_tree.to_conll(10).split('\n')[0]
+        expected = '1\tآدمک\tآدمک\tNOUN,EZ\tNOUN,EZ\t_\t4\tSBJ\t_\t_'
         assert actual == expected
