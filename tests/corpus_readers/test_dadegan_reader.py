e607304fa949b19b6b1646fb8399a502a34fb12f
E-Ghafour <E-Ghafour@users.noreply.github.com>
2023-07-05 12:38:16 +0000

    fix auto-fixable linting errors

--- tests/corpus_readers/test_dadegan_reader.py
+++ tests/corpus_readers/test_dadegan_reader.py
@@ -11,13 +11,13 @@ def dadegan_reader():
 def test_sents(dadegan_reader: DadeganReader):
     sents = dadegan_reader.sents()
     actual1 = next(sents)
-    expected1 = [('این', 'DET'), ('میهمانی', 'N'), ('به', 'P'), ('منظور', 'Ne'), ('آشنایی', 'Ne'), ('هم\u200cتیمی\u200cهای', 'Ne'), ('او', 'PRO'), ('با', 'P'), ('غذاهای', 'Ne'), ('ایرانی', 'AJ'), ('ترتیب', 'N'), ('داده_شد', 'V'), ('.', 'PUNC')]
+    expected1 = [("این", "DET"), ("میهمانی", "N"), ("به", "P"), ("منظور", "Ne"), ("آشنایی", "Ne"), ("هم\u200cتیمی\u200cهای", "Ne"), ("او", "PRO"), ("با", "P"), ("غذاهای", "Ne"), ("ایرانی", "AJ"), ("ترتیب", "N"), ("داده_شد", "V"), (".", "PUNC")]
     assert actual1 == expected1
 
 
 def test_trees(dadegan_reader: DadeganReader):
     trees = dadegan_reader.trees()
-    actual1 = next(trees).to_conll(10).split('\n')[0]
-    expected1 = '1\tاین\tاین\tPREM\tDEMAJ\tattachment=ISO|senID=23485\t2\tNPREMOD\t_\t_'
+    actual1 = next(trees).to_conll(10).split("\n")[0]
+    expected1 = "1\tاین\tاین\tPREM\tDEMAJ\tattachment=ISO|senID=23485\t2\tNPREMOD\t_\t_"
     assert actual1 == expected1
 
