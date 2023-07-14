358a3646798f29c6fbe0f92acc8f30bc9df7bb36
Ayub <sir.kokabi@gmail.com>
2023-07-14 11:41:43 +0330

    Add tests for wikipedia_reader

---
+++ tests/corpus_readers/test_wikipedia_reader.py
@@ -0,0 +1,18 @@
+import pytest
+
+from hazm import WikipediaReader
+
+
+@pytest.fixture(scope="module")
+def wikipedia_reader():
+    return WikipediaReader("wiki.xml.bz2")
+
+def test_docs(wikipedia_reader):
+    actual = next(wikipedia_reader.docs())["id"]
+    expected = "1"
+    assert actual == expected
+
+def test_texts(wikipedia_reader):
+    actual = next(wikipedia_reader.texts()).startswith("This is the first page of the Wikipedia")
+    expected = True
+    assert actual == expected
